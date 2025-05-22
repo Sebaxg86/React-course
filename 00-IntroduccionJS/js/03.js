7912b6da81c836998add9c380a88645a82a900ab
Sebastian Chairez <sebax86@icloud.com>
2025-05-22 17:13:38 -0600

    00 - Class 11, Data types

---
+++ 00-IntroduccionJS/js/03.js
@@ -0,0 +1,47 @@
+// Tipos de Datos
+
+//Undefined
+// let cliente;
+// console.log(cliente); 
+// console.log(typeof(cliente)); 
+
+//Strings o Cadenas de texto
+// let alumno ="Juan";
+// console.log(alumno);
+// console.log(typeof(alumno));
+
+// const producto = 'Monitor 20 pulgadas';
+// console.log(producto);
+// console.log(typeof(producto));
+
+// // Numbers
+// const numero1 = 1000;
+// const numero2 = 200;
+// const numero3 = 300.20;
+// console.log(typeof(numero1));
+// console.log(numero2);
+// console.log(numero3);
+
+// // Bigint
+// const numeroGrande = BigInt(1234567889234239423984238472397);
+// console.log(typeof(numeroGrande));
+
+// Boolean
+// const boolean1 = true;
+// const boolean2 = false;
+// console.log(boolean1);
+// console.log(boolean2);
+// console.log(typeof(boolean1));
+// console.log(typeof(boolean2));
+
+// // Null
+// const cliente = null;
+// console.log(cliente);
+// console.log(typeof(cliente)); // object
+
+// // Symbol
+// const primerSymbol = Symbol(30);
+// const segundoSymbol = Symbol(30);
+
+// console.log(primerSymbol == segundoSymbol); // false
+
