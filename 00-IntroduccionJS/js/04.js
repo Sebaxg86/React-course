6db0cc4d3d9e93d018d8b8ecbbb2eb8cb7c97914
Sebastian Chairez <sebax86@icloud.com>
2025-05-22 17:23:44 -0600

    00 - Class 12, Objects and Destructing

---
+++ 00-IntroduccionJS/js/04.js
@@ -0,0 +1,23 @@
+// Objetos
+// Un objeto es una colección de propiedades y métodos
+
+const producto = {
+    nombre: 'Tablet',
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto);
+console.log(typeof(producto)); // object
+console.table(producto); // Muestra el objeto en una tabla
+
+console.log(producto.nombre); // Acceder a la propiedad nombre
+
+//Destructuración de objetos
+//const {nombre} = producto
+const {nombre,precio,disponible} = producto;
+console.log(nombre); // Acceder a la propiedad nombre
+console.log(precio); // Acceder a la propiedad precio
+console.log(disponible); // Acceder a la propiedad disponible
+
+
