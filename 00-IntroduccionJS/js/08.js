94f9c6b23a416b879ed2c51e8f03c416d5fccc49
Sebastian Chairez <sebax86@icloud.com>
2025-05-22 20:49:40 -0600

    00 - Class 16, Template strings

---
+++ 00-IntroduccionJS/js/08.js
@@ -0,0 +1,13 @@
+// Template Strings y Concatenación
+
+const producto = "tablet de 12 pulgadas";
+const precio = 400;
+const marca = "Orange";
+
+console.log('El producto es: '+ producto);
+
+// Template strings
+console.log(`El producto es: ${producto}`);
+
+console.log(`${producto} $${precio} Dolares, marca: ${marca}`);
+
