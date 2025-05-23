e3892d15952012519359626cbc6718e796097f74
Sebastian Chairez <sebax86@icloud.com>
2025-05-23 00:17:53 -0600

    00 - Class 19, Destructuring Arrays

---
+++ 00-IntroduccionJS/js/11.js
@@ -0,0 +1,7 @@
+const tecnologias = ['HTML', 'CSS', 'Javascript', 'React.js', 'Node.js'];
+
+const react = tecnologias[3];
+
+//Destructuring arrays
+const [ , , , reactjs] = tecnologias;
+console.log(reactjs);
