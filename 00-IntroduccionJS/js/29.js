44e198ab7e5724a36d6a0a951f951bf578998916
Sebastian Chairez <sebax86@icloud.com>
2025-05-23 12:23:39 -0600

    00 - Class 41, DOM - Manipulate Elements

---
+++ 00-IntroduccionJS/js/29.js
@@ -0,0 +1,11 @@
+//Manipular elementos HTML con JS
+
+const heading = document.querySelector('.heading');
+const enlaces = document.querySelectorAll('.navegacion a');
+
+heading.textContent = 'Un nuevo heading...'
+
+const inputNombre = document.querySelector('#nombre');
+inputNombre.value = 'Un nuevo valor';
+
+enlaces[0].textContent = 'Nuevo Enlace';
