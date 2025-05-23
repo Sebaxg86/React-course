051524109135337cc86ecd8dc46ee7dc9561a52d
Sebastian Chairez <sebax86@icloud.com>
2025-05-23 00:29:55 -0600

    00 - Class 21, forEach | Map | for ... of

--- 00-IntroduccionJS/js/12.js
+++ 00-IntroduccionJS/js/12.js
@@ -1,5 +1,22 @@
 const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js', 'Nest.js', 'TypeScript'];
 
-for(let i = 0; i<tecnologias.length; i++){
-    console.log(tecnologias[i]);
+// for(let i = 0; i<tecnologias.length; i++){
+//     console.log(tecnologias[i]);
+// }
+
+//For each loop
+// tecnologias.forEach(function(tech){
+//     console.log(tech)
+// })
+
+//Map
+const arrayMap = tecnologias.map(function(tech){
+    return tech
+})
+
+//map generates a new array
+
+// For ... of
+for(let tech of tecnologias){
+    console.log(tech);
 }
