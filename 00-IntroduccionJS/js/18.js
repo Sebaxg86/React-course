fbc1c692273fa1e3a8e555413a1a4f66f80cd96a
Sebastian Chairez <sebax86@icloud.com>
2025-05-23 03:35:55 -0600

    00 - Class 28, Condicionals

---
+++ 00-IntroduccionJS/js/18.js
@@ -0,0 +1,14 @@
+// Condicionales
+const disponible = 40000;
+const retirar = 200;
+
+const auth = true;
+
+
+if(disponible >= retirar){
+    //Se cumple la condicion
+    console.log('Si puedes retirar');
+}else{
+    //No se cumple la condición
+    console.log('Saldo insuficiente');
+}
