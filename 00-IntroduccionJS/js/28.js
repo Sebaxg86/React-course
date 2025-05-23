710172ea053355619488c77f8584619430637126
Sebastian Chairez <sebax86@icloud.com>
2025-05-23 12:15:34 -0600

    00 - Class 40, DOM - Select Elements

---
+++ 00-IntroduccionJS/js/28.js
@@ -0,0 +1,9 @@
+// Selectores
+const heading = document.querySelector('.heading');
+
+// console.log(heading);
+// console.log(heading.tagName);
+// console.log(heading.textContent);
+
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces) 
