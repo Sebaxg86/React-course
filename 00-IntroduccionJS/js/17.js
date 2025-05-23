6b038cf83302926f5b991db98c9191d99564d617
Sebastian Chairez <sebax86@icloud.com>
2025-05-23 03:30:26 -0600

    00 - Class 27, Array Methods | Part 2 of 2

--- 00-IntroduccionJS/js/17.js
+++ 00-IntroduccionJS/js/17.js
@@ -6,6 +6,21 @@ const nuevoArray = tecnologias.filter((tech) => tech !== 'HTML');
 
 console.log(nuevoArray);
 
-//Includes
-const resultado = tecnologias.includes('CSS')
+// //Includes
+// const resultado = tecnologias.includes('CSS')
+// console.log(resultado);
+
+// Some - Devuelve si almenos uno cumple la condicion
+// const resultado = numeros.some(numeros => numero < 15);
+
+// Find - Devuelve el primer elemento que cumple una funcion
+// const resultado = numeros.find(numero => numero > 15);
+
+// Every - Retorna true o false si todos cumplen la condición
+// const resultado = numeros.every(numero => numero > 5);
+
+// Reduce - Retorna un acumulado del total
+const resultado = numeros.reduce((total, numero) => total + numero, 0)
+
 console.log(resultado);
+
