657f49510ee5d1a36bb5b67c44f05b5bb0b19ac4
Sebastian Chairez <sebax86@icloud.com>
2025-05-24 08:26:36 -0600

    01 - Class 64, Iterating on Products

--- 01-Project/guitarla/src/App.jsx
+++ 01-Project/guitarla/src/App.jsx
@@ -15,7 +15,9 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar />
+            {data.map(() => (
+                <Guitar />
+            ))}
         </div>
     </main>
 
